3d30c69b4b5bd701c9320d95248a1ebfa071c75f
wallace8bit <wallace8bit@yahoo.com>
2023-03-23 15:15:09 +0700

    a

---
+++ cctx/0.hello.ts
@@ -0,0 +1,41 @@
+// Example code in typescript
+// Based on /examples/js/fetch-from-many-exchanges-simultaneously.js
+
+import * as ccxt from 'ccxt';
+import fs from 'fs';
+
+const fetchSymbol = async () => {
+  const binanceUSDM = new ccxt.binanceusdm({
+    enableRateLimit: true,
+  });
+  binanceUSDM.setSandboxMode(true);
+  await binanceUSDM.loadMarkets();
+  const symbols = binanceUSDM.symbols;
+  console.log('🚀 ~ file: 0.hello.ts:16 ~ fetchTickers ~ symbols:', symbols);
+  fs.writeFileSync('cctx/binanceUSDM_symbols-2.txt', JSON.stringify(symbols));
+  //   binanceUSDM.;
+  //   const result = await Promise.all(
+  //     exchanges.map(async (id: string): Promise<ccxt.Exchange> => {
+  //       const CCXT = ccxt as any; // Hack!
+  //       const exchange = new CCXT[id]({ enableRateLimit: true }) as ccxt.Exchange;
+  //       const ticker = await exchange.fetchTicker(symbol);
+  //       const exchangeExtended = exchange.extend(
+  //         { exchange: id },
+  //         ticker,
+  //       ) as ccxt.Exchange;
+  //       return exchangeExtended;
+  //     }),
+  //   );
+  //   console.log('🚀 ~ file: 0.hello.ts:25 ~ fetchTickers ~ result:', result);
+};
+
+const fetchTicker = async (symbol: string) => {
+  const binanceUSDM = new ccxt.binanceusdm({
+    enableRateLimit: true,
+  });
+  binanceUSDM.setSandboxMode(true);
+  const ticker = await binanceUSDM.fetchTicker(symbol);
+  console.log('🚀 ~ file: 0.hello.ts:38 ~ fetchTicker ~ ticker:', ticker);
+  return ticker;
+};
+fetchTicker('XRP/USDT:USDT');
