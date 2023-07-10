a770b308717a9ee372c00213e052c05bc82746df
wallace8bit <thanhquisg24@gmail.com>
2023-07-10 12:57:27 +0700

    update app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,7 +4,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('App default')
-@Controller()
+@Controller('api/v1/app')
 export class AppController {
   constructor(
     private readonly appService: AppService,
@@ -14,11 +14,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    this.logger.log('Calling getHello()', AppController.name);
-    this.logger.error('Calling getHello()', AppController.name);
-    this.logger.debug('Calling getHello()', AppController.name);
-    this.logger.verbose('Calling getHello()', AppController.name);
-    this.logger.warn('Calling getHello()', AppController.name);
+    // this.logger.log('Calling getHello()', AppController.name);
+    // this.logger.error('Calling getHello()', AppController.name);
+    // this.logger.debug('Calling getHello()', AppController.name);
+    // this.logger.verbose('Calling getHello()', AppController.name);
+    // this.logger.warn('Calling getHello()', AppController.name);
     return this.appService.getHello();
   }
 }
